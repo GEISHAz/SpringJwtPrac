52b49898fb200748e01a277908c5a1392668818f
jungminho <allmin9702@naver.com>
2024-04-22 23:51:18 +0900

    Feat : 1 day

---
+++ springJWT/src/main/java/org/example/springjwt/controller/JoinController.java
@@ -0,0 +1,30 @@
+package org.example.springjwt.controller;
+
+import org.example.springjwt.dto.JoinDTO;
+import org.example.springjwt.service.JoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.xml.transform.OutputKeys;
+
+@Controller
+@ResponseBody
+public class JoinController {
+
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        System.out.println(joinDTO.getUsername());
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+}
